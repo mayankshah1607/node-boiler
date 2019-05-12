ad50f11458e6170a0baae326a8a30d156869ea5d
mayankshah1607 <mayankshah1614@gmail.com>
2019-05-12 18:16:14 +0530

    Testing contentmaker

--- test/contentTest.js
+++ test/contentTest.js
@@ -1,5 +1,20 @@
 const contentMaker = require('../helpers/contentMaker');
+const assert = require('chai').assert;
 
 describe('Testing functions in helpers/contentMaker', function(){
-    it('')
+    it('Testing models content', function(){
+        assert(contentMaker.modelsContent('hello'))
+    })
+
+    it('Testing views content', function(){
+        assert(contentMaker.modelsContent('hello'))
+    })
+
+    it('Testing controllers content', function(){
+        assert(contentMaker.modelsContent(['function1','function2']))
+    })
+
+    it('Testing routes content', function(){
+        assert(contentMaker.modelsContent({post: ['/r1','/r2'], get: ['/r3','/r4']}))
+    })
 })
